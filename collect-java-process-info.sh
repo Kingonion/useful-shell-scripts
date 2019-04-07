ce0ebac0c606fd9ad34ab81345a8142a16f749b0
Kingonion <neuwangcong@163.com>
2019-04-07 22:30:29 +0800

    add check method

--- collect-java-process-info.sh
+++ collect-java-process-info.sh
@@ -15,17 +15,54 @@ INTERVAL=1
 # total output count
 COUNT=5
 
-# binary of jstat
-JSTAT=jstat 
-
-# binary of jstack
-JSTACK=jstack 
-
-# binary of jmap
-JMAP=jmap
-
 function make_dirs() {
     [[ -d "${BASE_DIR}/output" ]] && mkdir -p "${BASE_DIR}/output"
+}
+
+# jps must be found
+function check_requirements() {
+    if [ -x $(command -v jps) ]
+    then 
+        JPS=jps
+    else
+        if [ -x $(command -v "${JAVA_HOME}/bin/jps") ] 
+        then 
+            JPS="${JAVA_HOME}/bin/jps"
+        else 
+            echo 'jps not found in $PATH or $JAVA_HOME/bin'
+            exit 1
+        fi
+    fi
+
+    if [ -x $(command -v jstack) ]
+    then 
+        JSTACK=jstack
+    else 
+        if [ -x $(command -v "${JAVA_HOME}/bin/jstack") ]
+        then 
+            JSTACK="${JAVA_HOME}/bin/jstack"
+        fi 
+    fi 
+
+    if [ -x $(command -v jstat) ]
+    then 
+        JSTAT=jstat
+    else 
+        if [ -x $(command -v "${JAVA_HOME}/bin/jstat") ]
+        then 
+            JSTAT="${JAVA_HOME}/bin/jstat"
+        fi 
+    fi 
+
+    if [ -x $(command -v jmap) ]
+    then 
+        JMAP=jmap
+    else 
+        if [ -x $(command -v "${JAVA_HOME}/bin/jmap") ]
+        then 
+            JMAP="${JAVA_HOME}/bin/jmap"
+        fi 
+    fi
 }
 
 # check if the process is a java process
@@ -34,6 +71,7 @@ function check_process() {
     local pid="${1}"
     if [[ ! $($JPS | awk '$1 == '"${pid}"' { print $1 }') ]]
     then
+        echo "pid[${pid}] not exists or not a java process"
         return 1
     fi
 }
@@ -41,7 +79,10 @@ function check_process() {
 # $1 pid
 function collect_top_info() {
     local pid="${1}"
-    top -b -n "${COUNT}" -d "${INTERVAL}" -H -p "${pid}" >> ${BASE_DIR}/output/${pid}/top.$(date +'%Y%m%d%H%M%S').txt
+    if [ ! -z "${pid}"]
+    then 
+        top -b -n "${COUNT}" -d "${INTERVAL}" -H -p "${pid}" >> ${BASE_DIR}/output/${pid}/top.$(date +'%Y%m%d%H%M%S').txt
+    fi 
 }
 
 # $1 pid
@@ -50,23 +91,33 @@ function collect_stack_info() {
     local time=$(date +'%Y%m%d%H%M%S')
     for i in $(seq ${COUNT})
     do
-        $JSTACK -F "${pid}" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
-        sleep ${INTERVAL}
-        echo "" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
-        echo "==================" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
+        if [ ! -z "${JSTACK}" ] && [ ! -z "${pid}" ]
+        then 
+            ${JSTACK} -F "${pid}" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
+            sleep ${INTERVAL}
+            echo "" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
+            echo "==================" >> ${BASE_DIR}/output/${pid}/jstack.$(date +'%Y%m%d%H%M%S').txt
+        fi 
     done
 }
 
 # $1 pid
 function collect_gc_info() {
     local pid="${1}"
-    $JSTAT -gcutil "${pid}" $[INTERVAL * 1000] ${COUNT} >> ${BASE_DIR}/output/${pid}/jstat.$(date +'%Y%m%d%H%M%S').txt
+    if [ ! -z "${JSTAT}" ] && [ ! -z "${pid}" ]
+    then 
+        ${JSTAT} -gcutil "${pid}" $[INTERVAL * 1000] ${COUNT} >> ${BASE_DIR}/output/${pid}/jstat.$(date +'%Y%m%d%H%M%S').txt
+    fi 
+    
 }
 
 # $1 pid
 function collect_heap_info() {
     local pid="${1}"
-    $JMAP -dump:format=b,file=${BASE_DIR}/output/${pid}/heap.$(date +'%Y%m%d%H%M%S').hprof "${pid}"
+    if [ ! -z "${JMAP}" ] && [ ! -z "${pid}" ]
+    then 
+        ${JMAP} -dump:format=b,file=${BASE_DIR}/output/${pid}/heap.$(date +'%Y%m%d%H%M%S').hprof "${pid}"
+    fi 
 }
 
 function export_functions() {
@@ -76,4 +127,4 @@ function export_functions() {
     export -f collect_heap_info
 }
 
-
+check_requirements
