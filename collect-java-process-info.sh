8983b0d052aba6cc71f3ac893bc3bce91996cd99
Kingonion <neuwangcong@163.com>
2019-04-09 21:36:39 +0800

    fix env not found

--- collect-java-process-info.sh
+++ collect-java-process-info.sh
@@ -12,15 +12,11 @@ BASE_DIR=$(cd $(dirname "$BASH_SOURCE[0]"); pwd)
 # keyword of java process
 KEYWORD=""
 
-# seconds between two outputs
-INTERVAL=1
+# seconds between two collections
+COLLECT_INTERVAL=1
 
-# total output count
-COUNT=5
-
-function make_dirs() {
-    [[ -d "${BASE_DIR}/output" ]] && mkdir -p "${BASE_DIR}/output"
-}
+# total count of the collections
+COLLECT_COUNT=5
 
 # jps must be found
 function check_requirements() {
@@ -68,44 +64,30 @@ function check_requirements() {
     fi
 }
 
-# check if the process is a java process
 # $1 pid
-function check_process() {
-    local pid="${1}"
-    if [[ ! $($JPS | awk '$1 == '"${pid}"' { print $1 }') ]]
-    then
-        echo "pid[${pid}] not exists or not a java process"
-        return 1
-    fi
-}
-
-# $1 pid
-# $2 main class name
 function collect_top_info() {
     local pid="${1}"
-    local main_class="${2}"
-    [[ -d "${BASE_DIR}/output/${main_class}" ]] || mkdir -p "${BASE_DIR}/output/${main_class}"
+    [[ -d "output/${pid}" ]] || mkdir -p "output/${pid}"
     if [ ! -z "${pid}" ]
     then 
-        top -b -n "${COUNT}" -d "${INTERVAL}" -H -p "${pid}" >> ${BASE_DIR}/output/${main_class}/top.$(date +'%Y%m%d%H%M%S').txt
+        top -b -n "${COLLECT_COUNT}" -d "${COLLECT_INTERVAL}" -H -p "${pid}" >> output/${pid}/top.$(date +'%Y%m%d%H%M%S').txt
     fi 
 }
 
 # $1 pid
 function collect_stack_info() {
     local pid="${1}"
-    local main_class="${2}"
     local time=$(date +'%Y%m%d%H%M%S')
-    [[ -d "${BASE_DIR}/output/${main_class}" ]] || mkdir -p "${BASE_DIR}/output/${main_class}"
-    for i in $(seq ${COUNT})
+    [[ -d "output/${pid}" ]] || mkdir -p "output/${pid}"
+    for i in $(seq ${COLLECT_COUNT})
     do
         if [ ! -z "${JSTACK}" ] && [ ! -z "${pid}" ]
         then 
-            ${JSTACK} -F "${pid}" >> ${BASE_DIR}/output/${main_class}/jstack.$(date +'%Y%m%d%H%M%S').txt
-            sleep ${INTERVAL}
-            echo "" >> ${BASE_DIR}/output/${main_class}/jstack.$(date +'%Y%m%d%H%M%S').txt
-            echo "==================" >> ${BASE_DIR}/output/${main_class}/jstack.$(date +'%Y%m%d%H%M%S').txt
-            echo "" >> ${BASE_DIR}/output/${main_class}/jstack.$(date +'%Y%m%d%H%M%S').txt
+            ${JSTACK} -F "${pid}" >> output/${pid}/jstack.${time}.txt
+            echo "" >> output/${pid}/jstack.${time}.txt
+            echo "==================" >> output/${pid}/jstack.${time}.txt
+            echo "" >> output/${pid}/jstack.${time}.txt
+            sleep ${COLLECT_INTERVAL}
         fi 
     done
 }
@@ -113,11 +95,10 @@ function collect_stack_info() {
 # $1 pid
 function collect_gc_info() {
     local pid="${1}"
-    local main_class="${2}"
-    [[ -d "${BASE_DIR}/output/${main_class}" ]] || mkdir -p "${BASE_DIR}/output/${main_class}"
+    [[ -d "output/${pid}" ]] || mkdir -p "output/${pid}"
     if [ ! -z "${JSTAT}" ] && [ ! -z "${pid}" ]
     then 
-        ${JSTAT} -gcutil "${pid}" $[INTERVAL * 1000] ${COUNT} >> ${BASE_DIR}/output/${main_class}/jstat.$(date +'%Y%m%d%H%M%S').txt
+        ${JSTAT} -gcutil "${pid}" $[COLLECT_INTERVAL * 1000] ${COLLECT_COUNT} >> output/${pid}/jstat.$(date +'%Y%m%d%H%M%S').txt
     fi 
     
 }
@@ -125,11 +106,10 @@ function collect_gc_info() {
 # $1 pid
 function collect_heap_info() {
     local pid="${1}"
-    local main_class="${2}"
-    [[ -d "${BASE_DIR}/output/${main_class}" ]] || mkdir -p "${BASE_DIR}/output/${main_class}"
+    [[ -d "output/${pid}" ]] || mkdir -p "output/${pid}"
     if [ ! -z "${JMAP}" ] && [ ! -z "${pid}" ]
     then 
-        ${JMAP} -dump:format=b,file=${BASE_DIR}/output/${main_class}/heap.$(date +'%Y%m%d%H%M%S').hprof "${pid}"
+        ${JMAP} -dump:format=b,file=output/${pid}/heap.$(date +'%Y%m%d%H%M%S').hprof "${pid}"
     fi 
 }
 
@@ -140,12 +120,22 @@ function export_functions() {
     export -f collect_heap_info
 }
 
+function export_envs() {
+    export JMAP
+    export JSTACK
+    export JSTAT
+    export JPS
+    export COLLECT_INTERVAL
+    export COLLECT_COUNT
+}
+
 function collect_java_process_info() {
+    cd ${BASE_DIR} &>/dev/null
     if [ "x${KEYWORD}" = "x" ]
     then 
         JAVA_PROCESSES="$(${JPS} -l | grep -v 'sun.tools.jps.Jps')"
     else 
-        JAVA_PROCESSES="$(${JPS} -l | grep ${KEYWORD} | grep -v grep)"
+        JAVA_PROCESSES="$(${JPS} -lmvV | grep ${KEYWORD} | grep -v grep)"
     fi 
     if [ "x{JAVA_PROCESSES}" = "x" ] 
     then 
@@ -157,16 +147,17 @@ function collect_java_process_info() {
     for process in $(echo "${JAVA_PROCESSES}")
     do
         local pid=$(echo "${process}" | awk '{ print $1 }')
-        local main_class=$(echo "${process}" | awk '{ print $2 }')
-        nohup bash -c "collect_top_info ${pid} ${main_class}" &>/dev/null &
-        nohup bash -c "collect_gc_info ${pid} ${main_class}" &>/dev/null &
-        nohup bash -c "collect_stack_info ${pid} ${main_class}" &>/dev/null &
-        nohup bash -c "collect_heap_info ${pid} ${main_class}" &>/dev/null &
+        nohup bash -c "collect_top_info ${pid}" &>/dev/null &
+        nohup bash -c "collect_gc_info ${pid}" &>/dev/null &
+        nohup bash -c "collect_stack_info ${pid}" &>/dev/null &
+        nohup bash -c "collect_heap_info ${pid}" &>/dev/null &
     done
 }
 
 check_requirements
 
 export_functions
+
+export_envs
 
 collect_java_process_info
